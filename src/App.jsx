fb9907d7ab8ccb8e0bba970a0f620cc08691613d
Divyansh Jha <divyanshjha30@gmail.com>
2023-08-23 16:12:16 +0530

    prettier format

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
-import MainLayout from "./components/MainLayout";
+import MainLayout from './components/MainLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<MainLayout/>}>
-
-        <Route path="/" element={<Home />} />
-        <Route path="/starred" element={<div>Starred page</div>} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/starred" element={<div>Starred page</div>} />
         </Route>
         <Route path="*" element={<div>Not Found</div>} />
 
