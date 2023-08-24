6dca3dbfce794641a3db8d7688a9707f8c7241f7
Divyansh Jha <divyanshjha30@gmail.com>
2023-08-25 04:32:27 +0530

    Subtitle Fixed

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Navs from './Navs';
 import AppTitle from './AppTitle';
-import SubTitle from './SubTitle'
 
 const MainLayout = () => {
   return (
     <div>
       <AppTitle />
-      <SubTitle />
       <Navs />
       <Outlet />
     </div>
